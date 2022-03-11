f4ddd5d7cd961ecd2073360a0160feb66e703bb4
Parkduksung <duksung1234@naver.com>
2022-03-11 20:59:56 +0900

    [feature] Splash 화면 구성.

---
+++ app/src/main/java/com/example/crimenotification/ui/splash/SplashActivity.kt
@@ -0,0 +1,15 @@
+package com.example.crimenotification.ui.splash
+
+import android.os.Bundle
+import com.example.crimenotification.R
+import com.example.crimenotification.base.BaseActivity
+import com.example.crimenotification.databinding.ActivitySplashBinding
+
+
+class SplashActivity : BaseActivity<ActivitySplashBinding>(R.layout.activity_splash) {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
