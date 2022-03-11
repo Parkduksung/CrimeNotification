3a3e4c79fc8d052fa9a511d1550b32d806899551
Parkduksung <duksung1234@naver.com>
2022-03-11 21:38:59 +0900

    [feature] kakaomap 추가.

--- app/src/main/java/com/example/crimenotification/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/crimenotification/ui/home/HomeActivity.kt
@@ -1,12 +1,41 @@
 package com.example.crimenotification.ui.home
 
+import android.annotation.SuppressLint
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Base64
+import android.util.Log
 import com.example.crimenotification.R
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        getHashKey()
+    }
+
+    @SuppressLint("PackageManagerGetSignatures")
+    private fun getHashKey() {
+        var packageInfo: PackageInfo? = null
+        try {
+            packageInfo =
+                packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNATURES)
+        } catch (e: PackageManager.NameNotFoundException) {
+            e.printStackTrace()
+        }
+        if (packageInfo == null) Log.e("KeyHash", "KeyHash:null")
+        for (signature in packageInfo!!.signatures) {
+            try {
+                val md: MessageDigest = MessageDigest.getInstance("SHA")
+                md.update(signature.toByteArray())
+                Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT))
+            } catch (e: NoSuchAlgorithmException) {
+                Log.e("KeyHash", "Unable to get MessageDigest. signature=$signature", e)
+            }
+        }
     }
 }
